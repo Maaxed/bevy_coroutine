2180f8e21012bbf69760ae8232b4d00330089193
Lucas Guedon <guedon.lucas@laposte.net>
2024-07-05 21:30:12 +0200

    Update to bevy 0.14 and add hurt_animation example

---
+++ examples/hurt_animation.rs
@@ -0,0 +1,85 @@
+use std::time::Duration;
+
+use bevy::prelude::*;
+use bevy::color::palettes::basic::*;
+use bevy::input::mouse::MouseButtonInput;
+use bevy::render::camera::ScalingMode;
+use bevy::asset::embedded_asset;
+use bevy_coroutine::prelude::*;
+
+fn main()
+{
+	App::new()
+		.add_plugins((
+			DefaultPlugins.set(ImagePlugin::default_nearest()),
+			CoroutinePlugin,
+			build_app,
+		))
+		.add_systems(Startup, setup_player)
+		.add_systems(Update, run_coroutines)
+		.run();
+}
+
+fn build_app(
+	app: &mut App,
+)
+{
+    embedded_asset!(app, "examples", "slime.png");
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup_player(
+	mut commands: Commands,
+	asset_server: Res<AssetServer>,
+)
+{
+	let mut camera = Camera2dBundle::default();
+	camera.projection.scaling_mode = ScalingMode::WindowSize(4.0);
+
+	commands.spawn(camera);
+	commands.spawn((
+		SpriteBundle
+		{
+        	texture: asset_server.load("embedded://hurt_animation/slime.png"),
+			..default()
+		},
+		Player
+	));
+}
+
+fn run_coroutines(
+	mut mouse_clicked: EventReader<MouseButtonInput>,
+	mut commands: Commands,
+	player: Query<Entity, With<Player>>,
+)
+{
+	for event in mouse_clicked.read()
+	{
+		if event.state.is_pressed()
+		{
+			let player = player.single();
+			// Launch a coroutine
+			commands.add(Coroutine::new(with_input(player, play_hurt_animation)));
+		}
+	}
+}
+
+fn play_hurt_animation(In(target): In<Entity>) -> CoResult {
+	// Start sub coroutines in sequence
+	co_break().with_subroutines((
+		with_input((target, RED.into()), set_sprite_color),
+		wait(Duration::from_secs_f32(0.1)),
+		with_input((target, WHITE.into()), set_sprite_color),
+	))
+}
+
+fn set_sprite_color(
+	In((entity, color)): In<(Entity, Color)>,
+	mut query: Query<&mut Sprite, With<Player>>,
+) -> CoResult {
+	let mut sprite = query.get_mut(entity).unwrap();
+	sprite.color = color;
+	co_break()
+}
