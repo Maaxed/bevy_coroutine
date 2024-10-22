85017190f7777d88bdf06f6df7e02d57d4a6535b
Nixon <voxell.technologies@gmail.com>
2024-10-22 10:38:19 +0800

    Undo to use tabs instead of spaces to match original convention

--- src/system_util.rs
+++ src/system_util.rs
@@ -1,15 +1,15 @@
-use bevy::ecs::system::{Adapt, AdapterSystem};
 use bevy::prelude::*;
+use bevy::ecs::system::{Adapt, AdapterSystem};
 
 /// Generates a system that executes the given system with the given input
 pub fn with_input<S, I, O, M>(input: I, system: S) -> AdapterSystem<InputAdapter<I>, S::System>
 where
-    S: IntoSystem<I, O, M>,
-    I: Sync + Send + Clone + 'static,
+	S: IntoSystem<I, O, M>,
+	I: Sync + Send + Clone + 'static,
 {
-    let system = IntoSystem::into_system(system);
-    let name = format!("with_input({})", system.name());
-    AdapterSystem::new(InputAdapter(input), system, name.into())
+	let system = IntoSystem::into_system(system);
+	let name = format!("with_input({})", system.name());
+	AdapterSystem::new(InputAdapter(input), system, name.into())
 }
 
 /// Used with [`AdapterSystem`] to pass an input to a system.
@@ -18,13 +18,14 @@ pub struct InputAdapter<I>(I);
 
 impl<S> Adapt<S> for InputAdapter<S::In>
 where
-    S: System,
-    S::In: Sync + Send + Clone,
+	S: System,
+	S::In: Sync + Send + Clone,
 {
-    type In = ();
-    type Out = S::Out;
+	type In = ();
+	type Out = S::Out;
 
-    fn adapt(&mut self, _input: (), run_system: impl FnOnce(S::In) -> S::Out) -> S::Out {
-        run_system(self.0.clone())
-    }
+	fn adapt(&mut self, _input: (), run_system: impl FnOnce(S::In) -> S::Out) -> S::Out
+	{
+		run_system(self.0.clone())
+	}
 }
